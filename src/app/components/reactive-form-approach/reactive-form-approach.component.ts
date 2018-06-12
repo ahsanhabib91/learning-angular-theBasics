4bfd71b4fe1c54e61d9cf093f9ec6c0937e9192d
ahsanhabib91 <habibibnakther@gmail.com>
2018-06-13 01:54:58 +0600

    changes done

--- src/app/components/reactive-form-approach/reactive-form-approach.component.ts
+++ src/app/components/reactive-form-approach/reactive-form-approach.component.ts
@@ -9,13 +9,14 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class ReactiveFormApproachComponent implements OnInit {
     genders: string[] = ['male', 'female'];
     signUpForm: FormGroup;
+    forbiddenUserNames = ['anna', 'henna'];
 
     constructor() { }
 
     ngOnInit() {
         this.signUpForm = new FormGroup({
             'userData': new FormGroup({
-                'username': new FormControl(null, Validators.required),
+                'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
                 'email': new FormControl(null, [Validators.required, Validators.email]),
             }),
             'description': new FormControl(null, Validators.required),
@@ -31,6 +32,14 @@ export class ReactiveFormApproachComponent implements OnInit {
     addHobby() {
         const control = new FormControl(null, Validators.required);
         (<FormArray>this.signUpForm.get('hobbies')).push(control);
+    }
+
+    forbiddenNames(control: FormControl): {[s: string]: boolean} {
+        if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+            return {'nameIsForbidden': true};
+        }
+        return null;
+        // return this.forbiddenUserNames.indexOf(control.value) !== -1 ? {'nameIsForbidden': true} : null;
     }
 
 }
